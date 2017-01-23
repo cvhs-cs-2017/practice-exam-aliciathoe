63f709dd35a84fb603cf582885782eb9c8d77c26
aliciathoe <thoeal112400@cvh.rusd.us>
2017-01-23 10:18:02 -0800

    loops

--- Functions.py
+++ Functions.py
@@ -1,9 +1,22 @@
 """1. Write a function that will double any integer (n) and return the result"""
 
+def Double(x):
+    x = x * 2
+    return x
 
+print(Double(4))
 
 
 
 """2.  Write a program that will (1) ask the user for an input value, (2) take
 that and double it and  (3) print the result.
 Include necessary print statements and address whitespace issues."""
+
+print ("Enter any value to double it.")
+x = int(input())
+
+def DoubleUser(x):
+    y = x * 2
+    print (y)
+
+DoubleUser(x)
--- Loops.py
+++ Loops.py
@@ -1,2 +1,13 @@
 """Use a loop to make a turtle draw a shape that is has at least 100 sides and
-that shows symmetry.  The entire shape must fit inside the screen""" 
+that shows symmetry.  The entire shape must fit inside the screen"""
+
+import turtle
+sven = turtle.Turtle()
+sven.speed(0)
+sven.pu()
+sven.goto(-100,150)
+sven.pd()
+for i in range(100):
+    sven.forward(100)
+    sven.right(43)
+input()
