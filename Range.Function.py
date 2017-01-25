7ef0e53a51c2478bb4e71b61889716f8e68dbad6
aliciathoe <thoeal112400@cvh.rusd.us>
2017-01-25 09:53:48 -0800

    range function

--- Range.Function.py
+++ Range.Function.py
@@ -1,9 +1,17 @@
 """Use the range function to print the numbers from 1-20"""
+'''
+for i in range(20):
+    i = i + 1
+    print (i)
 
+'''
 
 
 """Repeat the exercise above counting by 2's"""
-
-
-
+'''
+for i in range(2,22,2):
+    print (i)
+'''
 """Print all the multiples of 5 between 10 and 200 in DECENDING order"""
+for x in range(200, 10, -5):
+    print (x)
