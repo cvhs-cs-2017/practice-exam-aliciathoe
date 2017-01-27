05ff57502e3c87642ddf7fb4770cb52532877a8a
aliciathoe <thoeal112400@cvh.rusd.us>
2017-01-27 10:42:53 -0800

    encrypt

--- Encrypt.py
+++ Encrypt.py
@@ -1,2 +1,22 @@
 """Write a code that will take (AnyString) and substitute all vowels with
 character's like $#%^&* etc..... but DO NOT USE ' or " as any of your characters."""
+
+def AnyString(x):
+    newstring = ""
+    x = x.lower()
+    for ch in x:
+        if ord(ch) is 97:
+            newstring = newstring + '*'
+        elif ord(ch) is 101:
+            newstring = newstring + '^'
+        elif ord(ch) is 105:
+            newstring = newstring + '%'
+        elif ord(ch) is 111:
+            newstring = newstring + '$'
+        elif ord(ch) is 117:
+            newstring = newstring + '?'
+        else:
+            newstring = newstring + ch
+    return newstring
+
+print(AnyString("Alicia Bethany Thoe"))
