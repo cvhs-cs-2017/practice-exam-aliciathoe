484bff7f0f2b5577f1f86701fee18f9e8e61a6e7
aliciathoe <thoeal112400@cvh.rusd.us>
2017-01-30 10:28:08 -0800

    MyEncrypt

---
+++ MyEncrypt.py
@@ -0,0 +1,10 @@
+def MyEncrypt(x):
+    newstring = ""
+    for ch in x:
+        #print (ch, ord(ch))
+        y = ord(ch) + 5
+        newstring = newstring + chr(y)
+    return newstring
+
+
+print(MyEncrypt("Hello my name is Alicia."))
