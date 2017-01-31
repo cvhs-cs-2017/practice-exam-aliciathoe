e8ae844de76257522f055619ece453bebd904c78
aliciathoe <thoeal112400@CVH-RME120-12.cvh.rusd.us>
2017-01-31 10:40:37 -0800

    decryption

--- MyEncrypt.py
+++ MyEncrypt.py
@@ -8,3 +8,14 @@ def MyEncrypt(x):
 
 
 print(MyEncrypt("Hello my name is Alicia."))
+
+
+def MyDecrypt(x):
+    newstring = ""
+    for ch in x:
+        #print (ch, ord(ch))
+        y = ord(ch) - 5
+        newstring = newstring + chr(y)
+    return newstring
+
+print(MyDecrypt("Mjqqt%r~%sfrj%nx%Fqnhnf3"))
